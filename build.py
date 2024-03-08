778a50f051fa365fe8a3556f6c62a659960f7d45
Anirudh <anirudh.uppal@gmail.com>
2024-03-08 11:54:55 +0530

    Updated-12 app.py

--- build.py
+++ build.py
@@ -5,15 +5,15 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
-use_plugin("python.install_dependencies")
+# use_plugin("python.install_dependencies")
 
 name = "authenticate_and_content_retrieval"
-version = "0.1"
-summary = "Simple Web Application"
-description = "A simple web application using Flask"
-authors = ["Anirudh Uppal <anirudh.uppal@gmail.com>"]
-url = "https://github.com/anuppal/authenticate_and_content_retrieval"
-license = "MIT"
+# version = "0.1"
+# summary = "Simple Web Application"
+# description = "A simple web application using Flask"
+# authors = ["Anirudh Uppal <anirudh.uppal@gmail.com>"]
+# url = "https://github.com/anuppal/authenticate_and_content_retrieval"
+# license = "MIT"
 
 default_task = "publish"
 
